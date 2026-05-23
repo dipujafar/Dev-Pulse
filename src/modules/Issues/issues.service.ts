d60c365386b56000d380b5b745e8c3c8cc53e9f9
dipujafar <dipujafarmr@gmail.com>
2026-05-23 23:00:29 +0600

    write service for get single and delete a issue

--- src/modules/Issues/issues.service.ts
+++ src/modules/Issues/issues.service.ts
@@ -1,7 +1,6 @@
 import { pool } from "../../db/index.js";
 import type { IIssues } from "./issues.interface.js";
 
-
 const createIssueIntoDB = async (payload: IIssues, userId: string) => {
   const { title, description, type } = payload;
   const result = await pool.query(
@@ -13,6 +12,28 @@ const createIssueIntoDB = async (payload: IIssues, userId: string) => {
   return result;
 };
 
+const getSingleIssueFromDB = async (id: string) => {
+  const result = await pool.query(
+    `
+  SELECT * FROM issues WHERE id=$1
+`,
+    [id]
+  );
+  return result;
+};
+
+const deleteIssueFromDB = async (id: string) => {
+  const result = await pool.query(
+    `
+  DELETE FROM issue WHERE id=$1
+  `,
+    [id]
+  );
+  return result;
+};
+
 export const issuesService = {
-  createIssueIntoDB
-}
+  createIssueIntoDB,
+  getSingleIssueFromDB,
+  deleteIssueFromDB
+};
