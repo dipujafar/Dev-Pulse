8eb2359af08296e5096efd07d4c58f2252a8c147
dipujafar <dipujafarmr@gmail.com>
2026-05-24 00:26:38 +0600

    added controller for issue

--- src/modules/Issues/issues.controller.ts
+++ src/modules/Issues/issues.controller.ts
@@ -5,24 +5,44 @@ import type { Request, Response } from "express";
 
 const createIssue = async (req: Request, res: Response) => {
   try {
-      const {user_id} = req.user as JwtPayload;
-      const result = await issuesService.createIssueIntoDB(req.body, user_id);
-      sendResponse(res, {
-        statusCode: 201,
-        success: true,
-        message: "Issue created successfully",
-        data: result.rows[0],
-      });
-    } catch (error: any) {
-      sendResponse(res, {
-        statusCode: 500,
-        success: true,
-        message: error.message,
-        error: error,
-      });
-    }
-  };
-  
-  export const issueController = {
-    createIssue
+    const { user_id } = req.user as JwtPayload;
+    const result = await issuesService.createIssueIntoDB(req.body, user_id);
+    sendResponse(res, {
+      statusCode: 201,
+      success: true,
+      message: "Issue created successfully",
+      data: result.rows[0],
+    });
+  } catch (error: any) {
+    sendResponse(res, {
+      statusCode: 500,
+      success: true,
+      message: error.message,
+      error: error,
+    });
   }
+};
+
+const getAllIssues = async (req: Request, res: Response) => {
+  try {
+    const result = await issuesService.getAllIssuesFromDB(req.query);
+    sendResponse(res, {
+      statusCode: 200,
+      success: true,
+      message: "Issues retrived successfully",
+      data: result,
+    });
+  } catch (error: any) {
+    sendResponse(res, {
+      statusCode: 500,
+      success: true,
+      message: error.message,
+      error: error,
+    });
+  }
+};
+
+export const issueController = {
+  createIssue,
+  getAllIssues
+};
